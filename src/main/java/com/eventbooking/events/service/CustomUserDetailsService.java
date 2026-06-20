e932a6b5e723bc789062e75a6f4408bd53b18fcc
Michael Udo <mudo1@sheffield.ac.uk>
2026-06-20 14:05:57 +0100

    Added login authentication using UserDetailsService

---
+++ src/main/java/com/eventbooking/events/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.eventbooking.events.service;
+
+import com.eventbooking.events.model.User;
+import com.eventbooking.events.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).
+                orElseThrow(()-> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User( //I wrote the full import statement here so as to not avoid confusion with model.User
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
